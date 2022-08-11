5dbdad6f4aa18da15d0bea18cf34c6cc3d17c9f4
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2022-08-11 22:37:13 +0300

    Final

--- control.py
+++ control.py
@@ -13,20 +13,12 @@ def get_reads_dict():
     return reads_dict
 
 
-# data_1 = FileContent(get_reads_dict())
-# data_2 = FileContent(get_reads_dict())
-# data_3 = FileContent(get_reads_dict())
-#
-# read_n_dict = {data_1: data_1.calculate_read_n(), data_2: data_2.calculate_read_n(), data_3: data_2.calculate_read_n()}
-# read_n_dict = {key: value for key, value in sorted(read_n_dict.items(), key=lambda item: item[1])}
-# ui.print_all(next(iter(read_n_dict)))
-#
-# if next(iter(read_n_dict)) == 'data_1':
-#     ui.print_all(data_1)
-# elif next(iter(read_n_dict)) == 'data_2':
-#     ui.print_all(data_2)
-# elif next(iter(read_n_dict)) == 'data_3':
-#     ui.print_all(data_3)
-# print(data_2.data)
-# print(ui.print_all(data_2))
-print(get_reads_dict())
+data_1 = FileContent(get_reads_dict())
+data_2 = FileContent(get_reads_dict())
+data_3 = FileContent(get_reads_dict())
+
+# _list = [data1, data2, data3]
+# data_list = min(_list, key=lambda d: d.count_n_reads())
+read_n_dict = {data_1: data_1.count_n_reads(), data_2: data_2.count_n_reads(), data_3: data_3.count_n_reads()}
+read_n_dict = {key: value for key, value in sorted(read_n_dict.items(), key=lambda item: item[1])}
+ui.print_all(next(iter(read_n_dict)))
